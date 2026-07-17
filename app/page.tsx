602d3bc3dd06d9453ce2d1cf297229e02a7a0800
Mateusz Kurdyk <matic@MacBook-Air-Mateusz.local>
2026-07-17 13:15:08 +0200

    wersja mobilna, efekt dla przycisków v1.0

--- app/page.tsx
+++ app/page.tsx
@@ -9,18 +9,30 @@ export default function Home() {
 
   return (
     <div>
-      <header>
-        <Image
-          className="absolute -z-10"
-          src="/assets/images/background-pattern-desktop.svg"
-          alt="background"
-          width={1440}
-          height={200}
-          loading="eager"
-        />
+      <header className="relative w-full h-[200px] md:h-[300px] overflow-hidden">
+        
+          {/* Desktop */}
+          <Image
+            className="object-cover hidden md:block"
+            src="/assets/images/background-pattern-desktop.svg"
+            alt="background"
+            fill
+            priority
+          />
+          {/* Mobile */}
+          <Image
+            className="object-cover md:hidden"
+            src="/assets/images/background-pattern-mobile.svg"
+            alt="background"
+            fill
+            priority
+          />
+          {/* priority 
+          dla obrazów które są wyświetlane jako pierwsze np tło strony  */}
+        
       </header>
       <main className="flex align-center justify-center items-center">
-        <section className="flex-row mt-[180px] w-full max-w-[650px] bg-[white] text-[black] rounded-lg p-4 md:p-8">
+        <section className="flex-row relative z-10 -mt-[80px] m-5 md:-mt-[180px] w-full max-w-[650px] bg-[white] text-[black] rounded-lg p-4 md:p-8">
           <article className="flex align-center items-center">
             <Image src="/assets/images/icon-star.svg" alt="" width={40} height={40}  className="w-[40px] h-[40px]"/>
             <h1 className="text-5xl font-bold p-5">FAQs</h1>
@@ -31,27 +43,32 @@ export default function Home() {
             article key={index} uniklany klucz dla kazdego elementu ale nie jest to wzorowe rozwiązanie,
             lepiej uzyc unikalnego id w obiekcie FAQ i potem uzyc go jako key={faq.id}
           */}
+          {/* 
+          group w tailwind pozwala grupować elementy i np hover działa wtedy jako na jeden element całościowo
+          */}
 
           {faqs.map((faq, index) => (
-            <article key={index} className="flex flex-wrap align-center items-center justify-between p-4 border-b border-[hsl(275,10%,97%)]">
-              <h2 className="text-[hsl(292,42%,14%)] font-semibold">
+            <article key={index} className="flex flex-col border-b border-[hsl(275,10%,97%)]">
+              <button className="group flex grow-1 align-center items-center justify-between m-4" 
+              onClick={() => setActiveIndex(activeIndex === index ? null : index)}>
+              <h2 className="text-left text-[hsl(292,42%,14%)] font-semibold mr-10 group-hover:text-[hsl(292,58%,49%)]">
                 {faq.question}
               </h2>
-              <button onClick={() => setActiveIndex(activeIndex === index ? null : index)}>
+              
                 {/* jest to krótsze a zarazem wydajnieszje */}
                 <Image 
                   src={activeIndex === index ? "/assets/images/icon-minus.svg" : "/assets/images/icon-plus.svg"} 
                   alt="" 
                   width={30}
                   height={30} 
-                  className="w-[30px] h-[30px]"
+                  className=" w-[30px] h-[30px]"
                 />
                 {/* 
                 Jest lepszy sposób niz wgrywanie dwoch obrazków mozna je wgrywac zapomoca warunku w src
                 <Image src="/assets/images/icon-plus.svg" alt="" width={30} height={30} className={activeIndex === index ? "hidden" : "show"} />
                 <Image src="/assets/images/icon-minus.svg" alt="" width={30} height={30} className={activeIndex === index ? "show" : "hidden"} /> */}
               </button>
-              <p className={activeIndex === index ? "text-[hsl(292,16%,49%)] mt-6 mb-3 mr-9 font-normal" : "hidden"}>
+              <p className={activeIndex === index ? "text-[hsl(292,16%,49%)] mb-6 ml-4 mr-9 font-normal" : "hidden"}>
                 {faq.answer}
               </p>
             </article>
